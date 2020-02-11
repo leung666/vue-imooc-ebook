aefa70ded42921c4a6cbb52f819527d8a6eacde2
leung666 <15822198383@163.com>
2020-02-11 17:04:59 +0800

    第三章准备

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <span class="text">ABCDEFG</span>
     <router-view/>
   </div>
 </template>
+<script>
+import { mapGetters } from 'vuex'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  computed: {
+    ...mapGetters(['test'])
+  },
+  mounted () {
+    this.$store.dispatch('setTest', 9).then(() => {
+      console.log(this.test)
+    })
+  }
 }
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .text {
+    font-family: 'Days One';
+    font-size: px2rem(20);
+    color: orange;
+  }
 </style>
