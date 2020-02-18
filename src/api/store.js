bc1b751bded005d76dc5c3b04f992be7074048c9
leung666 <15822198383@163.com>
2020-02-18 15:22:05 +0800

    听书模块开发

--- src/api/store.js
+++ src/api/store.js
@@ -1,38 +1,10 @@
 import axios from 'axios'
 import { setLocalForage } from '../utils/localForage'
 
-export function download (book, onSuccess, onError, onProgress) {
-  if (!onProgress) {
-    onProgress = onError
-    onError = null
-  }
-  return axios.create({
-    baseURL: process.env.VUE_APP_EPUB_URL,
-    method: 'get',
-    responseType: 'blob',
-    timeout: 180 * 1000,
-    onDownloadProgress: progressEvent => {
-      if (onProgress) onProgress(progressEvent)
-    }
-  }).get(`${book.categoryText}/${book.fileName}.epub`)
-    .then(res => {
-      const blob = new Blob([res.data])
-      setLocalForage(book.fileName, blob, () => {
-        if (onSuccess) {
-          onSuccess(book)
-        }
-      }, err => {
-        if (onError) onError(err)
-      })
-    }).catch(err => {
-      if (onError) onError(err)
-    })
-}
-
-export function home () {
+export function flatList () {
   return axios({
     method: 'get',
-    url: `${process.env.VUE_APP_BASE_URL}/book/home`
+    url: `${process.env.VUE_APP_BOOK_URL}/book/flat-list`
   })
 }
 
@@ -40,6 +12,13 @@ export function shelf () {
   return axios({
     method: 'get',
     url: `${process.env.VUE_APP_BASE_URL}/book/shelf`
+  })
+}
+
+export function home () {
+  return axios({
+    method: 'get',
+    url: `${process.env.VUE_APP_BASE_URL}/book/home`
   })
 }
 
@@ -58,4 +37,28 @@ export function list () {
     method: 'get',
     url: `${process.env.VUE_APP_BASE_URL}/book/list`
   })
+}
+
+export function download (book, onSucess, onError, onProgress) {
+  if (onProgress == null) {
+    onProgress = onError
+    onError = null
+  }
+  return axios.create({
+    baseURL: process.env.VUE_APP_EPUB_URL,
+    method: 'get',
+    responseType: 'blob',
+    timeout: 180 * 1000,
+    onDownloadProgress: progressEvent => {
+      if (onProgress) onProgress(progressEvent)
+    }
+  }).get(`${book.categoryText}/${book.fileName}.epub`)
+    .then(res => {
+      const blob = new Blob([res.data])
+      setLocalForage(book.fileName, blob,
+        () => onSucess(book),
+        err => onError(err))
+    }).catch(err => {
+      if (onError) onError(err)
+    })
 }
