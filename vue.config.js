af0b33586451f8e74f49a2816942fc03ae770918
leung666 <15822198383@163.com>
2020-02-20 23:39:15 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -22,8 +22,8 @@ module.exports = {
   configureWebpack: {
     performance: {
       hints: 'warning',
-      maxAssetSize: 524288,
-      maxEntrypointSize: 524288
+      maxAssetSize: 524288 * 10,
+      maxEntrypointSize: 524288 * 10
     }
   }
 }
